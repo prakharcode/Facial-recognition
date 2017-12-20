30a17f719212e4e646b879fc5e5f9881ae2eb5fa
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2017-12-20 14:42:54 +0530

    idk2

--- __init__.py
+++ __init__.py
@@ -11,36 +11,40 @@ app.config.from_object(Configuration)
 @app.route('/',methods = ['GET','POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'FILE NOT UPLOADED'
-        f = request.files['file']
-        file_data = f.stream.read()
-        nparr = np.fromstring(file_data, np.uint8)
-        img_color = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        img = cv2.imdecode(nparr, 0)
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        img_str = cv2.imencode('.jpg', img_color)[1].tostring()
-        encoded = base64.b64encode(img_str)
-        mime = "image/jpg"
-        mime = mime + ";"
-        input_image = "data:%sbase64,%s" % (mime, encoded)
-        faces = face_cascade.detectMultiScale(img, 1.3, 5)
-        result = []
-        if faces!=():
-            for (x,y,w,h) in faces:
-                cv2.rectangle(img_color,(x,y),(x+w,y+h),(255,255,0),2)
-                detected_face = img[y:y+h, x:x+w]
-                # detected_face = facealign(detected_face)
-                result.append(Recognize(detected_face))
-            print result
+        try:
+            if 'file' not in request.files:
+                return 'FILE NOT UPLOADED'
+            f = request.files['file']
+            file_data = f.stream.read()
+            nparr = np.fromstring(file_data, np.uint8)
+            img_color = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            img = cv2.imdecode(nparr, 0)
             img_str = cv2.imencode('.jpg', img_color)[1].tostring()
             encoded = base64.b64encode(img_str)
             mime = "image/jpg"
             mime = mime + ";"
             input_image = "data:%sbase64,%s" % (mime, encoded)
-            return render_template('result.html', success=True, input_image=input_image, face_detected=result[0], face_detected_length=len(result))
-        else:
-            return render_template('result.html', success=False, input_image=input_image)
+            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            faces = face_cascade.detectMultiScale(img, 1.3, 5)
+            result = []
+            if faces!=():
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(img_color,(x,y),(x+w,y+h),(255,255,0),2)
+                    detected_face = img[y:y+h, x:x+w]
+                    # detected_face = facealign(detected_face)
+                    result.append(Recognize(detected_face))
+                print result
+                img_str = cv2.imencode('.jpg', img_color)[1].tostring()
+                encoded = base64.b64encode(img_str)
+                mime = "image/jpg"
+                mime = mime + ";"
+                input_image = "data:%sbase64,%s" % (mime, encoded)
+                return render_template('result.html', success=True, input_image=input_image, face_detected=result[0], face_detected_length=len(result))
+
+            else:
+                return render_template('result.html', success=False, input_image=input_image)
+        except:
+            pass
     return render_template('modi.html')
 
 
